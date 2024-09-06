5229657d74297305f049cf3b8a1db353db7abb3b
cybertfort <104222482+cybertfort@users.noreply.github.com>
2024-09-06 07:17:57 -0500

    Update looping.java
    Adding additional comments

--- looping.java
+++ looping.java
@@ -23,8 +23,9 @@ public class Looping {
 	            }
 	                
 	            count++;
-	        } else {
-	            System.out.println("Invalid input. Please enter a floating-point value.");
+	       //Input Validation
+	       } else {
+	          System.out.println("Invalid input. Please enter a floating-point value.");
 	            scanner.next();
 	            }
 	        }
@@ -41,6 +42,5 @@ public class Looping {
 	        System.out.println("Minimum: " + min);
 	        System.out.println("Interest on total at 20%: " + interest);
 
-	        scanner.close();
 	    }
 	}
