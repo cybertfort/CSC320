8039fce2ce50c7dccf24959f17864de09a9c6c2f
cybertfort <104222482+cybertfort@users.noreply.github.com>
2024-10-02 07:54:21 -0500

    Create name.java

---
+++ name.java
@@ -0,0 +1,21 @@
+
+public class Mod_1_CSC320 {
+
+	public static void main(String[] args) {
+
+		String firstName = "Tyler";
+		String lastName = "Fortner";
+		String Address = "123 Main St.";
+		String City = "Cedar Rapids";
+		String zipCode = "52402";
+		
+		   
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + Address);
+		System.out.println("City: " + City);
+		System.out.println("Zip Code: " + zipCode);
+		
+	}
+
+}
